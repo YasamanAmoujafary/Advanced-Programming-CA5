5391c2a3636ba5114c5f9f39c1a27c5e70372df1
YasamanAmoujafary <yasamanamoujafary@gmail.com>
2024-05-10 11:43:56 +0330

    start to make the zombies animation and their classes

--- header/includes.hpp
+++ header/includes.hpp
@@ -52,6 +52,7 @@ const string REGULAR_PROJECTILE_PNG = "regular_projectile.png";
 const string SNOW_PROJECTILE_PNG= "snow_projectile.png";
 const string REGULAR_PROJECTILE_NAME = "regular_projectile";
 const string SNOW_PROJECTILE_NAME = "snow_projectile";
+const string REGULAR_ZOMBIE_PNG = "regular_zombie";
 
 
 
--- header/zombie.hpp
+++ header/zombie.hpp
@@ -4,12 +4,26 @@
 class Zombie
 {
 public:
+    Zombie(RenderWindow *_window_ptr, string zombie_type_png) : window_ptr(_window_ptr)
+    {
+        if(!zombie_texture.loadFromFile(PICS_PATH + zombie_type_png))
+        {
+           cerr << "cant upload image!";
+        }
+        zombie_sprite.setTexture(zombie_texture);
+        zombie_sprite.setScale(1,1);
+        IntRect rect;
+        rect.width = 34;
+        rect.height = 62;
+        zombie_sprite.setTextureRect(rect);
+    }
     // virtual void render();
     // virtual void update();
 
 protected:
     Texture zombie_texture;
     Sprite zombie_sprite;
+    RenderWindow *window_ptr;
     int health;
     int damage;
     int speed;
